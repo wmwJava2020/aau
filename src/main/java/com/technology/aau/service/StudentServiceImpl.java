121919b0fb00e985ec755f0afc2060171a78c572
jack <wondafrashmw@gmail.com>
2026-07-08 10:29:55 -0500

    first code commit-update logger for ServiceImpl class

--- src/main/java/com/technology/aau/service/StudentServiceImpl.java
+++ src/main/java/com/technology/aau/service/StudentServiceImpl.java
@@ -7,8 +7,12 @@ import com.technology.aau.utility.StudentEnum;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class StudentServiceImpl implements StudentService {
+
+    Logger log = Logger.getLogger(StudentServiceImpl.class.getName());
 
     private final ModelMapper modelMapper;
     private final StudentRepository studentRepository;
@@ -24,19 +28,22 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public StudentResponse createStudent(Student req) {
         Student student = modelMapper.map(req, Student.class);
+        log.info("Creating student: " + student);
         Student saved = studentRepository.save(student);
+        log.info("Student created: " + saved);
         return modelMapper.map(saved, StudentResponse.class);
     }
 
     @Override
     public void delete(Student student) {
         studentRepository.delete(student);
-
+        log.info("Student deleted: " + student);
     }
 
     @Override
     public int grade(int creditHours, int contactHours, int assignmentScore) {
         try {
+            log.info("Calculating grade for student with credit hours: " + creditHours + ", contact hours: " + contactHours + ", assignment score: " + assignmentScore);
             if (!StudentEnum.ACTIVE.equals(StudentEnum.ACTIVE)) {
             } else {
                 return (creditHours * contactHours) + assignmentScore;
