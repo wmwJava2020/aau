525dca441ef9dea96c4a8e885a36b2ac1a72aa1b
jack <wondafrashmw@gmail.com>
2026-07-08 11:40:39 -0500

    code commit for Exception class-updated

--- src/main/java/com/technology/aau/utility/StudentException.java
+++ src/main/java/com/technology/aau/utility/StudentException.java
@@ -5,12 +5,11 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
-@Component
-public class StudentException extends RuntimeException{
+public class StudentException extends RuntimeException {
 
-
+    String message;
     public StudentException(String messages) {
         super(messages);
     }
-    
+
 }
